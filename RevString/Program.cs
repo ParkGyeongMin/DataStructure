18f8a5996677c971a7e18191aef42da78c12b986
ParkGyeongMin <dlfjsskawk00@naver.com>
2019-03-07 23:18:53 +0900

    add RevString

---
+++ RevString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace RevString
+{
+    internal class Program
+    {
+        static void ReversetStr(char[] str)
+        {
+            int length = str.Length;
+            for (int i = 0; i < length/2; i++)
+            {
+                char temp = str[i];
+                str[i] = str[length - i - 1];
+                str[length - i - 1] = temp;
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+
+            Console.Write("원하는 문자열을 입력하세요.");
+            string input = Console.ReadLine();
+            char[] str = input.ToCharArray();
+            
+            ReversetStr(str);
+            
+            foreach (var s in str)
+            {
+                Console.Write(s);
+            }
+
+        }
+    }
+}
